d4e845de9d9f1729aa6ba8a825fe5ea8a83d7aa1
Victor Mesquita <mesquitadev@gmail.com>
2024-09-04 12:57:48 -0300

    chore: removendo codigo nao usado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -126,7 +126,7 @@ export default function Home() {
                 colorScheme={'pink'}
                 leftIcon={<FaInstagram />}
                 as={Link}
-                href="https://www.instagram.com/formen_mutimarcas?igsh=MWV1eWsyNHZod2Fmcg=="
+                href="https://www.instagram.com/formen_mulltimarcas?igsh=MTcxNDB0bTNpNm5uZA=="
                 target="_blank"
               >
                 <Center>
